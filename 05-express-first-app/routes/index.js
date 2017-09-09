e6d59b9345677486e944bcb7143f092ceac5ee01
Rupesh Basnet <rbasnet000@citymail.cuny.edu>
2017-09-09 18:34:05 -0400

    PugJS Templating Engine

--- 05-express-first-app/routes/index.js
+++ 05-express-first-app/routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'A new title', condition: true});
+});
+
+/* GET users listing. */
+router.get('/users', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/users/detail', function(req, res, next) {
+  res.send('detail');
 });
 
 module.exports = router;
